e0c2ca986eefec64b69e2bcd05f013190b643fb4
vanek021 <www.vanek01@mail.ru>
2021-04-27 01:49:21 +0500

    style chnges

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,7 +1,7 @@
 function call_click() {
     console.log('click')
     const counter = document.getElementById('counter')
-    counter.innerText = parseInt(counter.innerText) + 1
+    counter.textContent = (parseInt(counter.innerText) + 1) + " $"
     var x = new XMLHttpRequest();
     x.open("GET", "api/call_click/", true);
     x.send();
